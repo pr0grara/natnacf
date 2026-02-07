d838f6737dd5cecd780aab1c03559bfc2f227e40
Ara Z Baghdassarian <azbaghda@gmail.com>
2026-02-06 23:56:31 -0800

    swap css with scss, update header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import natna_logo from '../assets/natna_logo.png';
 
 export default function Header() {
   return (
     <header className="site-header compact">
       <div className="container nav-bar">
-        <Link to="/" className="logo">Natna</Link>
+        {/* <img src={natna_logo} alt="" className="logo" /> */}
+        <Link to="/" className=""><img src={natna_logo} alt="" className="logo" /></Link>
         <nav className="nav-links">
-          <Link to="/work">Home</Link>
+          <Link to="/">Home</Link>
           <Link to="/about">About Us</Link>
           <Link to="/missions">Programs & Projects</Link>
-          <Link to="/donate">Donate</Link>
           <Link to="/contact">Contact Us</Link>
+          <Link to="/donate">Donate</Link>
         </nav>
       </div>
     </header>
